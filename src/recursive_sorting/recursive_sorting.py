5c6d1d3596c35f2a957edf7dea0f5408adde5c00
Paulus Kutrich <pkutrich@gmail.com>
2019-11-05 16:53:45 -0800

    Add stretch

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,8 +1,10 @@
+from bisect import bisect_right as br
+
+
 # TO-DO: complete the help function below to merge 2 sorted arrays
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-    print(arrA, arrB)
     for i in range(elements):
         if not arrA:
             merged_arr[i] = arrB.pop(0)
@@ -12,7 +14,6 @@ def merge(arrA, arrB):
             merged_arr[i] = arrA.pop(0)
         elif arrB[0] < arrA[0]:
             merged_arr[i] = arrB.pop(0)
-    print('m', merged_arr)
     return merged_arr
 
 
@@ -29,15 +30,19 @@ def merge_sort(arr):
 
 
 # STRETCH: implement an in-place merge sort algorithm
-def merge_in_place(arr, start, mid, end):
-    # TO-DO
+def merge_in_place(arrA, arrB):
+    for x in arrB:
+        arrA.insert(br(arrA, x), x)
+    return arrA
 
-    return arr
 
-
-def merge_sort_in_place(arr, l, r): 
-    # TO-DO
-
+def merge_sort_in_place(arr, l, r):
+    len_arr = len(arr)
+    if len_arr > 1:
+        split = len_arr // 2
+        left = merge_sort_in_place(arr[:split], l, r)
+        right = merge_sort_in_place(arr[split:], l, r)
+        arr = merge_in_place(left, right)
     return arr
 
 
